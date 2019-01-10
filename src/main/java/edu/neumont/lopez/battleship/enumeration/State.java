648724ff633eb7e8512837c64341b2bc361251c3
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-01-10 01:33:30 -0700

    Update in Ship
    Added more functionality to the Ship class

--- src/main/java/edu/neumont/lopez/battleship/enumeration/State.java
+++ src/main/java/edu/neumont/lopez/battleship/enumeration/State.java
@@ -1,8 +1,22 @@
 package edu.neumont.lopez.battleship.enumeration;
 
 public enum State {
-    UNHIT,
-    HIT,
-    MISS,
-    EMPTY
+    UNHIT('S'),  //S = Ship
+    HIT('H'),
+    MISS('M'),
+    EMPTY('~');
+
+    private char status;
+
+    State(char status) {
+        this.setStatus(status);
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
 }
--- src/main/java/edu/neumont/lopez/battleship/model/Ship.java
+++ src/main/java/edu/neumont/lopez/battleship/model/Ship.java
@@ -6,30 +6,48 @@ import edu.neumont.lopez.battleship.enumeration.State;
 public class Ship {
     private final String name;
     private final int size;
-    private final int lives;
+    private int lives;
+    private boolean isSunk;
 
     public Ship(Ships ship) {
         this.name = ship.getName();
         this.size = ship.getSize();
+        this.isSunk = false;
         this.lives = size;
     }
 
-//    public State getState(){
-//        if(lives == 0){
-//            return State.DESTROYED;
-//        } else if(lives < size){
-//            return State.HITTED;
-//        } else {
-//            return State.MISSED;
-//        }
-//    }
+    public char getState() {
+        if (lives == 0) {
+            return 'X';
+        } else if (lives < size) {
+            return State.HIT.getStatus();
+        } else {
+            return State.MISS.getStatus();
+        }
+    }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
+    public boolean isSunk() {
+        return isSunk;
+    }
+
+    public void setSunk(boolean sunk) {
+        isSunk = sunk;
+    }
+
+    public void shipHitred() {
+        if (lives == 0) {
+            isSunk = true;
+            System.out.println("You sunk the " + name);
+            return;
+        }
+        lives--;
+    }
 }
